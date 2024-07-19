6294a4231d126acb16012a348d2433103cd63849
TYHH10 <10300731+TYHH10@user.noreply.gitee.com>
2024-07-19 12:12:31 +0800

    鏇存柊缈昏瘧

--- up.bat
+++
@@ -1,3 +0,0 @@
-git add -A
-git commit -m '更新翻译'
-git push -f git@github.com:ignis05/baro-save-editor.git master:gh-pages
---
+++ up.ps1
@@ -0,0 +1,4 @@
+git add -A
+git commit -m '更新翻译'
+git push -f git@github.com:ignis05/baro-save-editor.git master:gh-pages
+fSdt_yCp-WwX69yNfmc-
