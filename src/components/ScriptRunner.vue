6b7ab3edf505a32cc3855bd657220302f86a199f
TYHH10 <10300731+TYHH10@user.noreply.gitee.com>
2024-11-08 14:24:20 +0800

    更改一下

--- src/components/ScriptRunner.vue
+++ src/components/ScriptRunner.vue
@@ -24,7 +24,7 @@ export default {
       eval(this.areaText)
       this.$store.dispatch('showAlert', {
         type: 'success',
-        text: `成功运行脚本(检查浏览器控制台)`,
+        text: `成功运行脚本(请查看浏览器控制台)`,
       })
     },
   },
